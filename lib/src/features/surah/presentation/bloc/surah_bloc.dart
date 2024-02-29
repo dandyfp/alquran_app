8cc5b58b0df1cfd0f143b1d7343e19f95be532ef
dandyfirmansyah <dandyfp@sakutambah.id>
2024-03-01 00:29:15 +0700

    Refactor: main dart

--- lib/src/features/surah/presentation/bloc/surah_bloc.dart
+++ lib/src/features/surah/presentation/bloc/surah_bloc.dart
@@ -1,3 +1,6 @@
+import 'package:alquran_app/src/core/service/request_state.dart';
+import 'package:alquran_app/src/features/surah/domain/entities/surah.dart';
+import 'package:alquran_app/src/features/surah/domain/usecases/get_surah.dart/get_surah.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -5,9 +8,14 @@ part 'surah_event.dart';
 part 'surah_state.dart';
 
 class SurahBloc extends Bloc<SurahEvent, SurahState> {
-  SurahBloc() : super(SurahInitial()) {
-    on<SurahEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetSurah _getSurah;
+  SurahBloc(this._getSurah) : super(SurahInitial()) {
+    on<SurahEvent>((event, emit) async {
+      final result = await _getSurah.call(null);
+      result.fold(
+        (l) => emit(SurahFailure(RequestState.error(l.getErrorMessage()))),
+        (r) => emit(SurahLoaded(r)),
+      );
     });
   }
 }
--- lib/src/features/surah/presentation/bloc/surah_state.dart
+++ lib/src/features/surah/presentation/bloc/surah_state.dart
@@ -1,9 +1,22 @@
 part of 'surah_bloc.dart';
 
 abstract class SurahState extends Equatable {
-  const SurahState();  
+  const SurahState();
 
   @override
   List<Object> get props => [];
 }
+
 class SurahInitial extends SurahState {}
+
+class SurahFailure extends SurahState {
+  final RequestState requestState;
+
+  const SurahFailure(this.requestState);
+}
+
+class SurahLoaded extends SurahState {
+  final Surah data;
+
+  const SurahLoaded(this.data);
+}
