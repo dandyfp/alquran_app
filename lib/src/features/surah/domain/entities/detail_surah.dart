2f0d92c2be8b05f40d24380bfc293ffed2019965
dandyfirmansyah <dandyfp@sakutambah.id>
2024-03-04 07:35:08 +0700

    Feat: detail surah

---
+++ lib/src/features/surah/domain/entities/detail_surah.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class DetailSurah extends Equatable {
+  final int? number;
+  final int? sequence;
+
+  const DetailSurah(this.number, this.sequence);
+
+  @override
+  List<Object?> get props => [];
+}
